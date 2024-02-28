5c1541c0b1e2e9f71658af899fe3fa9f8351889d
Pratik Sangle <pratiksangle6@gmail.com>
2024-02-27 19:33:24 -0500

    Trying to fix entire code

--- server/index.js
+++ server/index.js
@@ -33,11 +33,11 @@ app.use(
     })
 );
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 //globals here
 //This file contains all global variables
 //Example: constants, variable names, etc.
